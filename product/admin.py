946ef2f86b8401045a2c340b66f1e0b7daa57c39
VarunRele <rele.varun1@gmail.com>
2025-07-11 16:56:45 +0530

    3rd commit

--- product/admin.py
+++ product/admin.py
@@ -1,10 +1,21 @@
 from django.contrib import admin
 from .models import Product, ProductImage
+from django.utils.html import format_html
+
+
+class ProductImageInline(admin.TabularInline):
+    model = ProductImage
+    extra = 1
+    readonly_fields = ['image_preview']
+
+    def image_preview(self, obj):
+        if obj.image:
+            return format_html('<img src="{}" width="100" height="100" />', obj.image.url)
+        return ""
+    image_preview.short_description = 'Preview'
+
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ["title", "description", "price"]
-
-@admin.register(ProductImage)
-class ProductImageAdmin(admin.ModelAdmin):
-    pass
+    inlines = [ProductImageInline]
--- product/serializer.py
+++ product/serializer.py
@@ -2,12 +2,13 @@ from rest_framework import serializers
 from .models import Product, ProductImage
 
 class ProductImageSerializer(serializers.ModelSerializer):
+    image = serializers.ImageField(use_url=True, read_only=True)
     class Meta:
         model = ProductImage
         fields = ['image']
 
 class ProductSerializer(serializers.ModelSerializer):
-    image = ProductImageSerializer(many=True, read_only=True)
+    images = ProductImageSerializer(many=True, read_only=True)
     class Meta:
         model = Product
         fields = [
@@ -15,5 +16,5 @@ class ProductSerializer(serializers.ModelSerializer):
             "title",
             "description",
             "price",
-            "image"
+            "images"
         ]
